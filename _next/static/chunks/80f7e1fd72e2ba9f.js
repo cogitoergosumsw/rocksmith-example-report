5e5940c89b495b851a3ae38c72221424472500b8
samwell <sengwee.samwell@gmail.com>
2025-10-30 16:38:13 +0800

    init assets

---
+++ _next/static/chunks/80f7e1fd72e2ba9f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/a63490dc81920ab6.js",
+  "static/chunks/aecc9f4ca4f17d1e.js",
+  "static/chunks/turbopack-9feb2d337cec65bc.js"
+])
---
+++ _next/static/chunks/b75bcf0bb4827990.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/912da6ffb11f5b36.js",
+  "static/chunks/aecc9f4ca4f17d1e.js",
+  "static/chunks/turbopack-51d0bdc3cb10ef07.js"
+])
